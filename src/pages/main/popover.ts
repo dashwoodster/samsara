f66fd194dd8e9a4e90589a35d4c14ab8698b7b25
Dashwood <dashwoodster@users.noreply.github.com>
2017-03-31 19:06:43 -0400

    Commit

--- src/pages/main/popover.ts
+++ src/pages/main/popover.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
-import { ViewController } from 'ionic-angular';
+import { ViewController, AlertController } from 'ionic-angular';
+import * as Bcryptjs from "bcryptjs";
+
 @Component({
   templateUrl: 'popover.html'
 })
 
 export class GodMode {
-constructor(public viewCtrl: ViewController) {}
-close() {
+  
+  public hash: string;
+  public result: boolean;
+
+  constructor(public viewCtrl: ViewController, public alertCtrl: AlertController) { }
+
+  authentication() {
+    let prompt = this.alertCtrl.create({
+      title: 'Gode Mode Authentication',
+      message: "Only the heads of the Scouting Team can access this",
+      inputs : [
+        {
+          name: 'Password',
+          type: 'password',
+          placeholder: 'Password'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Exited Godmode prompt');
+            this.viewCtrl.dismiss();
+          }
+        },
+        {
+          text: 'Authenticate',
+          handler: data =>{
+            this.result = Bcryptjs.compareSync(data, "$2a$06$BLaLTSB8AixB.bd4OLNKuOc.CXMS/I27haszcgITbN6niSuAZhv1y");
+            console.log(this.result);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  close() {
     this.viewCtrl.dismiss();
   }
 }
